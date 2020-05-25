166b5f8ac9d77a4bc8c63d8c43fa44598be40b5b
Bijay Sharma <yoursbijay@outlook.com>
2020-05-25 09:58:38 +0530

    added comments to selection sort source code for better understanding

--- selection_sort.c
+++ selection_sort.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<conio.h>
 
+//selection sort function
 void sort(int arr[], int s){
 	
 	int temp;
@@ -24,8 +25,11 @@ void sort(int arr[], int s){
 
 void main(){
  
+	// let us take any array a
 	int a[] = {55,45,95,68,72,33,42,12,66,10,3,88,101,21};
+	// size of this array is 4
 
+	// displaying the values befor sorting
 	printf("Array before sorting : ");
 	for(int i=0; i < 14; i++){
 		printf("%d, ", a[i]);
@@ -36,6 +40,7 @@ void main(){
 	//sort
 	sort(a, 14);
 
+	//displaying the vlaues after sorting
 	printf("Array after sorting : ");
 	for(int i=0; i < 14; i++){
 		
