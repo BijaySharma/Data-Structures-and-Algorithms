37bd5a51c187eb1a2deb9fa858e394be22181960
Bijay Sharma <yoursbijay@outlook.com>
2020-05-25 09:50:33 +0530

    selection sort added

---
+++ selection_sort.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+
+void sort(int arr[], int s){
+	
+	int temp;
+	
+	for(int i = 0; i < (s-1); i++){
+		
+		for(int j = i; j < s; j++){
+		
+			if(arr[j] < arr[i]){
+				//swap
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		
+		}	
+
+	}	
+
+}
+
+void main(){
+ 
+	int a[] = {55,45,95,68,72,33,42,12,66,10,3,88,101,21};
+
+	printf("Array before sorting : ");
+	for(int i=0; i < 14; i++){
+		printf("%d, ", a[i]);
+	}
+
+	printf("\n");
+
+	//sort
+	sort(a, 14);
+
+	printf("Array after sorting : ");
+	for(int i=0; i < 14; i++){
+		
+		printf("%d, ", a[i]);
+	
+	}
+	printf("\n");
+	
+	getch();
+				       
+}
