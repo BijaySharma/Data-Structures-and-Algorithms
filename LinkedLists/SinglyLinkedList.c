16ed48d651c860f795264a250ad1ec641957d9d1
Bijay Sharma <yoursbijay@outlook.com>
2020-10-08 14:02:58 +0000

    Singly Linked List with Tail

--- LinkedLists/SinglyLinkedList.c
+++ LinkedLists/SinglyLinkedList.c
@@ -19,6 +19,12 @@ struct Node *getNewNode(int data){
 }
 
 void print(struct List list){
+  
+  if(list.head == NULL){
+    printf("List is Empty \n");
+    return;
+  }
+
   struct Node *p = list.head;
   while(p != NULL){
     printf("%d ", p->data);
@@ -50,20 +56,82 @@ void insertAtHead(struct List *list, int data){
   list->head = newNode;
 }
 
-// TODO Delete
-// void delete(struct List *list, int index){
-//   if(list->head == NULL){
-//     printf("Error: List is empty. \n");
-//     return;
-//   }
 
-//   int i;
-//   struct Node *p = list->head;
-//   for(i = 1; i<index; i++){
-//     p = p->next;
-//   }
+void delete(struct List *list, int index){
+  if(list->head == NULL){
+    printf("Error: List is empty. \n");
+    return;
+  }
 
-// }
+  struct Node *p, *temp;
+
+  if(index == 1){
+    temp = list->head;
+    list->head = temp->next;
+    
+    if(list->tail == temp)
+      list->tail = list->head;
+    
+    free(temp);
+    return;
+  }
+
+  int i;
+  p = list->head;
+  for(i = 1; i<(index - 1); i++){
+    p = p->next;
+  }
+  
+  if(p == NULL || p->next == NULL){
+    printf("Invaid Index \n");
+  }
+
+  temp = p->next;
+  p->next = temp->next;
+
+  if(list->tail == temp)
+    list->tail = p;
+
+  free(temp);
+}
+
+void deleteFromHead(struct List *list){
+  if(list->head == NULL){
+    printf("Error: List is empty. \n");
+    return;
+  }
+
+  if(list->head == list->tail)
+    list->tail = NULL;
+
+  struct Node *temp = list->head;
+  list->head = list->head->next;
+  free(temp);
+}
+
+void deleteFromTail(struct List *list){
+  if(list->head == NULL || list->tail == NULL){
+    printf("Error: List is empty. \n");
+    return;
+  }
+
+  if(list->head == list->tail){
+    free(list->head);
+    list->head = NULL;
+    list->tail = NULL;
+  }
+
+  int i;
+  struct Node *p = list->head;
+  
+  for(i = 1; p->next != list->tail; i++)
+    p = p->next;
+  
+  free(list->tail);
+  p->next = NULL;
+  list->tail = p;
+
+}
 
 
 int main(){
@@ -74,5 +142,20 @@ int main(){
   insertAtTail(&list, 7);
   insertAtHead(&list, 9);
   print(list);
+  delete(&list, 5);
+  print(list);
+  insertAtTail(&list, 99);
+  print(list);
+  delete(&list, 1);
+  print(list);
+  deleteFromTail(&list);
+  print(list);
+  deleteFromHead(&list);
+  print(list);
+  deleteFromHead(&list);
+  print(list);
+  deleteFromHead(&list);
+  print(list);
+
   return 0;
 }
